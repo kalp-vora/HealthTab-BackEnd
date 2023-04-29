ac44d9df4d21be27fcfcfc699cce54037cd65579
kalp-vora <kalp.vora09@gmail.com>
2023-04-30 01:56:47 +0530

    Added Doctor.java

---
+++ src/main/java/com/kalptree/healthtab/entity/Doctor.java
@@ -0,0 +1,38 @@
+package com.kalptree.healthtab.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+import static com.kalptree.healthtab.constant.Constants.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Integer doctorId;
+
+    @NotEmpty(message = REQUIRED_LICENCE)
+    @Column(nullable = false, unique = true, length = 30)
+    private String licenseNumber;
+
+    @NotEmpty(message = REQUIRED_OLD_CASE_FEES)
+    @Column(nullable = false)
+    private Integer oldCaseFees;
+
+    @NotEmpty(message = REQUIRED_NEW_CASE_FEES)
+    @Column(nullable = false)
+    private Integer newCaseFees;
+
+    @OneToOne
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private Users userId;
+}
